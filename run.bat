29562f134cc8462c74a0a5e13b76bfa0b4266beb
unknown <viniciusferreirawk@gmail.com>
2018-04-04 23:48:14 -0300

    fix

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
-#SET ambientePath = /c/Users/veni/Desktop/docker/php56docker
 docker run -d -i  ^
 	   --name php56docker ^
 	   -p 8000:80 ^
 	   -v /c/Users/veni/Desktop/docker/php56docker/www:/var/www/html ^
-	   -v /c/Users/veni/Desktop/docker/php56docker/logs:/var/log/httpd/ ^
+	   -v /c/Users/veni/Desktop/docker/php56docker/logs:/var/log/httpd ^
 veni:php56docker
